64090c73c91ec11e19522dac47e6fbeb5244c82d
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-09-24 13:54:32 +0700

    แก้ adsblock

--- src/components/adsblock.vue
+++ src/components/adsblock.vue
@@ -1,46 +1,55 @@
 <template>
   <div class="thumbnailDiv">
-    รูปโฆษณา {{ data }}
     <div class="row items-center">
-      <div class="col-5">
+      <div class="col-6 q-pl-md">
         <div class="headerText">{{ title }}</div>
-        <div>URL ปลายทาง</div>
+        <div>URL ปลายทาง {{ target }}</div>
       </div>
-      <div class="headerBox row col-4" align="center">
+      <div class="headerBox row items-center col-4" align="center">
         <div class="col-3 q-pa-sm">
-          <div>วันที่ใช้</div>
-          <div>64 วัน</div>
+          <div class="borderR">
+            <div>วันที่ใช้</div>
+            <div>64 วัน</div>
+          </div>
         </div>
         <div class="col-3">
-          <div>น้ำหนัก</div>
-          <div>25%</div>
+          <div class="borderR">
+            <div>น้ำหนัก</div>
+            <div>{{ weight }}%</div>
+          </div>
         </div>
-        <div class="col-6">640 คลิ๊ก</div>
+        <div class="col-6" style="font-size:35px;">{{ statview }} คลิ๊ก</div>
       </div>
-      <div class="col-3 row" align="center">
-        <div class="underLine row col" align="center">แก้ไข</div>
-        <div class="col">
-          <q-btn color="positive" label="ออนไลน์" rounded />
+      <div class="col-2 row" align="center">
+        <div class="underLine col">
+          แก้ไข
+        </div>
+        <div v-show="status == 1" class="col btgreen" @click="onlineBtn">
+          ออนไลน์
+        </div>
+        <div v-show="status == 0" class="col btred" @click="offlineBtn">
+          ออฟไลน์
         </div>
       </div>
     </div>
-    <div class="brx">เส้นกั้น</div>
-    <div class="row q-pa-sm items-center">
+    <div class="q-pt-sm"><hr style="width:98%" /></div>
+    <div class="row q-pa-md items-center">
       <div class="col-2">สำหรับ Mobile</div>
       <div><img :src="filePic01" alt="" /></div>
     </div>
     <div class="row q-pa-sm items-center">
       <div class="col-2">สำหรับ Tablet</div>
-      <div><img src="../../public/images/tabletPic.jpg" alt="" /></div>
+      <div><img :src="filePic02" alt="" /></div>
     </div>
     <div class="row q-pa-sm items-center">
       <div class="col-2">สำหรับ PC</div>
-      <div><img src="../../public/images/pcPic.jpg" alt="" /></div>
+      <div><img :src="filePic03" alt="" /></div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   props: {
     data: Object,
@@ -52,11 +61,41 @@ export default {
       filePic02: "",
       filePic03: "",
       title: "",
-      id: ""
+      id: "",
+      target: "",
+      weight: 0,
+      status: 0,
+      statview: 0
     };
+  },
+  methods: {
+    async onlineBtn() {
+      this.status = 0;
+      let temp = {
+        status: 0,
+        at_id: this.id
+      };
+      let url = this.serverpath + "bo_ads_on_off_line.php";
+      let res = await axios.post(url, JSON.stringify(temp));
+    },
+    async offlineBtn() {
+      this.status = 1;
+
+      let temp = {
+        status: 1,
+        at_id: this.id
+      };
+      let url = this.serverpath + "bo_ads_on_off_line.php";
+      let res = await axios.post(url, JSON.stringify(temp));
+    }
   },
   mounted() {
     this.title = this.data.at_title;
+    this.target = this.data.at_target;
+    this.weight = this.data.at_weight;
+    this.statview = this.data.statview;
+    this.status = this.data.status;
+    this.id = this.data.at_id;
     this.filePic01 =
       this.serverpath +
       "ads/" +
@@ -64,10 +103,29 @@ export default {
       "/mobile.jpg?x=" +
       Math.floor(Math.random() * 999) +
       100;
+    this.filePic02 =
+      this.serverpath +
+      "ads/" +
+      this.data.at_folder +
+      "/tablet.jpg?x=" +
+      Math.floor(Math.random() * 999) +
+      100;
+    this.filePic03 =
+      this.serverpath +
+      "ads/" +
+      this.data.at_folder +
+      "/pc.jpg?x=" +
+      Math.floor(Math.random() * 999) +
+      100;
   },
   watch: {
     data: function(newData, oldData) {
       this.title = newData["at_title"];
+      this.target = newData["at_target"];
+      this.weight = newData["at_weight"];
+      this.statview = newData["statview"];
+      this.status = newData["status"];
+      this.id = newData["at_id"];
       this.filepic01 =
         this.serverpath +
         "ads/" +
@@ -76,6 +134,20 @@ export default {
         Math.floor(Math.random() * 999) +
         100;
 
+      this.filepic02 =
+        this.serverpath +
+        "ads/" +
+        newData["at_folder"] +
+        "/tablet.jpg?x=" +
+        Math.floor(Math.random() * 999) +
+        100;
+      this.filepic03 =
+        this.serverpath +
+        "ads/" +
+        newData["at_folder"] +
+        "/pc.jpg?x=" +
+        Math.floor(Math.random() * 999) +
+        100;
       this.id = newData["at_id"];
     }
   }
@@ -98,5 +170,24 @@ export default {
 }
 .underLine {
   text-decoration: underline;
+}
+.borderR {
+  border-right: 1px solid white;
+}
+.btgreen {
+  width: 70%;
+  background-color: #9eee39;
+  color: black;
+  text-align: center;
+  border-radius: 50px;
+  cursor: pointer;
+}
+.btred {
+  width: 70%;
+  background-color: #ff2070;
+  color: black;
+  text-align: center;
+  border-radius: 50px;
+  cursor: pointer;
 }
 </style>
